d79117bbfb86a274277cba6cf4a542536ca8a5f0
Nolan Gelinas <nolan.gelinas@duke.edu>
2022-01-25 10:45:04 -0500

    Refactor to useState

--- src/components/WalletConnectButton/index.js
+++ src/components/WalletConnectButton/index.js
@@ -4,11 +4,6 @@ import Web3Modal from "web3modal";
 import React, { useState } from "react";
 import Web3 from "web3";
 
-let provider;
-let web3Modal;
-let currentAccount;
-let setConnectedAccount;
-
 const providerOptions = {
 	walletconnect: {
 		package: WalletConnectProvider,
@@ -18,77 +13,72 @@ const providerOptions = {
 	},
 };
 
-web3Modal = new Web3Modal({
+const web3Modal = new Web3Modal({
 	cacheProvider: false, // optional
 	providerOptions, // required
 });
 
-async function onConnect() {
-	console.log("Opening a dialog", web3Modal);
-	try {
-		provider = await web3Modal.connect();
+const WalletConnectButton = () => {
+	const [accounts, setAccounts] = useState(null);
+	const [chainId, setChainId] = useState();
+	const [networkId, setNetworkId] = useState();
+	let provider;
+
+	async function fetchAccountData() {
+		// Get a Web3 instance for the wallet
 		const web3 = new Web3(provider);
+
+		console.log("Web3 instance is", web3);
+
+		// Get connected chain id from Ethereum node
+		const chainId = await web3.eth.getChainId();
+		console.log(chainId);
+		// Load chain information over an HTTP API
+
+		// Get list of accounts of the connected wallet
 		const accounts = await web3.eth.getAccounts();
-		currentAccount = accounts[0];
-	} catch (e) {
-		console.log("Could not get a wallet connection", e);
-		return false;
+
+		// MetaMask does not give you all accounts, only the selected account
+		console.log("Got accounts", accounts);
+		setAccounts(accounts);
 	}
 
-	// Subscribe to accounts change
-	provider.on("accountsChanged", (accounts) => {
-		console.log(accounts);
-	});
+	async function onConnect() {
+		console.log("Opening a dialog", web3Modal);
+		try {
+			provider = await web3Modal.connect();
+		} catch (e) {
+			console.log("Could not get a wallet connection", e);
+			return false;
+		}
 
-	// Subscribe to chainId change
-	provider.on("chainChanged", (chainId) => {
-		console.log(chainId);
-	});
+		// Subscribe to accounts change
+		provider.on("accountsChanged", (accounts) => {
+			console.log("Accounts: ", accounts);
+			setAccounts(accounts);
+		});
 
-	// Subscribe to networkId change
-	provider.on("networkChanged", (networkId) => {
-		console.log(networkId);
-	});
+		// Subscribe to chainId change
+		provider.on("chainChanged", (chainId) => {
+			console.log("ChainID: ", chainId);
+			setChainId(chainId);
+		});
 
-	await fetchAccountData();
+		// Subscribe to networkId change
+		provider.on("networkChanged", (networkId) => {
+			console.log("Network Id: ", networkId);
+			setNetworkId(networkId);
+		});
 
-	return true;
-}
+		await fetchAccountData();
+	}
 
-async function fetchAccountData() {
-	// Get a Web3 instance for the wallet
-	const web3 = new Web3(provider);
+	const WalletConnected = () => (
+		<div className={styles.connectedContainer}>{accounts[0]}</div>
+	);
 
-	console.log("Web3 instance is", web3);
-
-	// Get connected chain id from Ethereum node
-	const chainId = await web3.eth.getChainId();
-	console.log(chainId);
-	// Load chain information over an HTTP API
-
-	// Get list of accounts of the connected wallet
-	const accounts = await web3.eth.getAccounts();
-
-	// MetaMask does not give you all accounts, only the selected account
-	console.log("Got accounts", accounts);
-	currentAccount = accounts[0];
-}
-
-const WalletConnected = () => (
-	<div className={styles.connectedContainer}>{currentAccount}</div>
-);
-
-const WalletConnectButton = () => {
-	const [connectedAccount, setConnectedAccount] = useState("");
-
-	return connectedAccount === "" ? (
-		<button
-			onClick={() => {
-				let response = onConnect();
-				setConnectedAccount(response);
-			}}
-			id={styles.WalletConnectButton}
-		>
+	return accounts === null ? (
+		<button onClick={onConnect} id={styles.WalletConnectButton}>
 			Connect Wallet
 		</button>
 	) : (
